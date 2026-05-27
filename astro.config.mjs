3101fd5052679749c21defaca013d321760b9320
jukogo <jukogo@gmail.com>
2026-05-27 21:24:35 +0200

    Eliminar adaptador inexistente @astrojs/github y usar output static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
 export default defineConfig({
-  adapter: github({
-    site: 'https://jukogo.github.io/sirilastro-es', // URL donde se servirá la página
-  }),
+  site: 'https://jukogo.github.io/sirilastro-es',
   output: 'static',
 });
