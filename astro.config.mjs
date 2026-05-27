b372bdb422938ea4048e46c76aa445a2199f644b
jukogo <jukogo@gmail.com>
2026-05-27 20:46:05 +0200

    Configurar Astro para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import github from '@astrojs/github';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: github({
+    site: 'https://jukogo.github.io/sirilastro-es', // URL donde se servirá la página
+  }),
+  output: 'static',
+});
