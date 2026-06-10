869a750c26d79c6f6d39cde41dceb9563071eee9
jukogo <jukogo@gmail.com>
2026-06-10 17:08:01 +0200

    Limpiar restos de Astro y código no usado

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://jukogo.github.io/sirilastro-es',
-  base: '/sirilastro-es',
-  output: 'static',
-});
